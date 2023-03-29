0fa127b971bfd6f532491d575d87641a4f499f9f
mikeAdamss <mikelovesbooks@gmail.com>
2023-03-29 11:52:06 +0100

    add a healthcheck to keep k8s happy

--- temporary_app.py
+++ temporary_app.py
@@ -4,11 +4,24 @@ on port 3000.
 """
 
 from flask import Flask
+from flask import Response
 
 app = Flask(__name__)
 
+@app.route("/_healthcheck")
+def health_check():
+    """
+    Add a simple healthcheck to keep k8s happy, else the
+    app will run be perpetually reported as being in a state
+    of 0/1 pods ready.
+    """
+    return Response("", status=200)
+
 @app.route("/")
 def all():
+    """
+    Catch everything that's not a healthcheck
+    """
     return "Hello world! I will be a prototype"
 
 if __name__ == "__main__":
