cd2635971f37d5f04b23efc7936508825f9fb7af
Rhys St Romaine <rhys.st.romaine@ons.gov.uk>
2023-04-20 14:00:31 +0100

    dom fix

--- data-catalogue/src/components/Pagination/index.js
+++ data-catalogue/src/components/Pagination/index.js
@@ -1,18 +1,18 @@
 export default function Pagination(props) {
   return (
     <nav
-      class="govuk-pagination govuk-pagination--block"
+      className="govuk-pagination govuk-pagination--block"
       role="navigation"
       aria-label="results"
     >
       {props.page - 1 > 0 && (
         <div
-          class="govuk-pagination__prev"
+          className="govuk-pagination__prev"
           onClick={() => props.changePage(-1)}
         >
-          <a class="govuk-link govuk-pagination__link" rel="prev" href="#">
+          <a className="govuk-link govuk-pagination__link" rel="prev" href="#">
             <svg
-              class="govuk-pagination__icon govuk-pagination__icon--prev"
+              className="govuk-pagination__icon govuk-pagination__icon--prev"
               xmlns="http://www.w3.org/2000/svg"
               height="13"
               width="15"
@@ -22,25 +22,25 @@ export default function Pagination(props) {
             >
               <path d="m6.5938-0.0078125-6.7266 6.7266 6.7441 6.4062 1.377-1.449-4.1856-3.9768h12.896v-2h-12.984l4.2931-4.293-1.414-1.414z"></path>
             </svg>
-            <span class="govuk-pagination__link-title">Previous</span>
-            <span class="govuk-visually-hidden">:</span>
-            <span class="govuk-pagination__link-label">
+            <span className="govuk-pagination__link-title">Previous</span>
+            <span className="govuk-visually-hidden">:</span>
+            <span className="govuk-pagination__link-label">
               {props.page - 1} of {props.totalPages}
             </span>
           </a>
         </div>
       )}
       {props.page + 1 < props.totalPages && (
-        <div class="govuk-pagination__next">
+        <div className="govuk-pagination__next">
           <a
-            class="govuk-link govuk-pagination__link"
+            className="govuk-link govuk-pagination__link"
             rel="next"
             onClick={() => props.changePage(1)}
             href="#"
           >
             {" "}
             <svg
-              class="govuk-pagination__icon govuk-pagination__icon--next"
+              className="govuk-pagination__icon govuk-pagination__icon--next"
               xmlns="http://www.w3.org/2000/svg"
               height="13"
               width="15"
@@ -50,9 +50,9 @@ export default function Pagination(props) {
             >
               <path d="m8.107-0.0078125-1.4136 1.414 4.2926 4.293h-12.986v2h12.896l-4.1855 3.9766 1.377 1.4492 6.7441-6.4062-6.7246-6.7266z"></path>
             </svg>{" "}
-            <span class="govuk-pagination__link-title">Next</span>
-            <span class="govuk-visually-hidden">:</span>
-            <span class="govuk-pagination__link-label">
+            <span className="govuk-pagination__link-title">Next</span>
+            <span className="govuk-visually-hidden">:</span>
+            <span className="govuk-pagination__link-label">
               {props.page + 1} of {props.totalPages}
             </span>
           </a>
